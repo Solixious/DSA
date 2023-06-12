b42b7851fc99a917a9546dce6fe3bc973ba4a32c
Pratyush Choudhary <solixious@gmail.com>
2023-06-12 18:58:53 +0400

    Added a simple stack class

--- src/model/ds/Stack.java
+++ src/model/ds/Stack.java
@@ -9,6 +9,26 @@ public class Stack<T> {
     private Integer top;
 
     public Stack() {
+        this.stack = new ArrayList<>();
+    }
 
+    public Stack(final Integer size) {
+        this.stack = new ArrayList<>(size);
+        this.size = size;
+    }
+
+    public void push(T data) {
+        stack.add(data);
+        top++;
+    }
+
+    public T pop() {
+        T data = stack.get(top - 1);
+        stack.remove(--top);
+        return data;
+    }
+
+    public T peek() {
+        return stack.get(top - 1);
     }
 }
