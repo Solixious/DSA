7ce2512d6bd3825ef8170312917cf45b4f10ca2f
Pratyush Choudhary <solixious@gmail.com>
2023-06-27 08:58:36 +0400

    Empty implementation of queue using linked list

---
+++ src/ds/queue/impl/QueueLinkedListImplementation.java
@@ -0,0 +1,26 @@
+package ds.queue.impl;
+
+import ds.queue.IQueue;
+
+public class QueueLinkedListImplementation implements IQueue {
+
+    @Override
+    public void enqueue(Object data) {
+
+    }
+
+    @Override
+    public Object dequeue() {
+        return null;
+    }
+
+    @Override
+    public Boolean isEmpty() {
+        return null;
+    }
+
+    @Override
+    public Integer size() {
+        return null;
+    }
+}
